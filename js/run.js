7cf60a0dfce0e4f4f086718165f73f45ef124e9f
Pey-crypto <53893419+Pey-crypto@users.noreply.github.com>
2021-06-10 10:13:16 +0530

    Cleaned the data ex animation

--- js/run.js
+++ js/run.js
@@ -4,27 +4,29 @@ function confulence(jsonData){
     var latitude = jsonData.latitude;
     var longi = jsonData.longitude;
     var ip = jsonData.ip;
-    document.getElementById('scr').innerHTML ="Loading code into memory....<br>" +"Country: "+country +"<br>"
-    +"Your public ip address: "+ip+"<br>"
-    +"Your Internet Service Provider: "+isp+"<br>"
-    +"Longititude: "+longi+"<br>"
-    +"Latitude: "+latitude+"<br>";
-}
+    document.getElementById('scr').innerHTML ="Loading code into memory....<br>";
+    document.getElementById('acr').innerHTML ="Country: "+country+"<br>";
+    document.getElementById('bcr').innerHTML ="Internet Service Provider: "+isp+"<br>"; 
+    document.getElementById('ccr').innerHTML ="IP Address: "+ip+"<br>"; 
+    document.getElementById('dcr').innerHTML ="Latitude: "+latitude+"<br>";
+    document.getElementById('ecr').innerHTML ="longitude: "+longi+"<br>";
+     
+  }
 
 
 function bimp(addr){
     var get =" https://ipapi.co/" + addr+"/json/";
-    console.log(get);
     fetch(get)
     .then(function(response) {
       response.json().then(jsonData => {
         confulence(jsonData);
-        
       });
     })
     .catch(function(error) {
       console.log(error)
-    });}
+    });
+  
+  }
 function getIP(json){
     var addr = json.ip;
     bimp(addr);
