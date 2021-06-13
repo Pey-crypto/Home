f19288c8a09de24d3a0b097f7a86d26adcee12b9
Pey-crypto <53893419+Pey-crypto@users.noreply.github.com>
2021-06-13 16:31:08 +0530

    The browser exploit check has been enabled

--- js/run.js
+++ js/run.js
@@ -1,3 +1,34 @@
+function brew(){
+  var len = navigator.appVersion.length;
+  var edg = navigator.appVersion.indexOf("Edg");
+  var chr = navigator.appVersion.indexOf("Chrome");
+  var saf = navigator.appVersion.indexOf("Safari");
+
+  var str = navigator.appVersion;
+  var res = str.substring(edg,len);
+  var res1 = str.substring(chr,saf);
+  var res2 = str.substring(saf,edg);
+  console.log("Edge: "+res);
+  console.log("Chrome: "+res1);
+  console.log("Safari : "+res2);
+  var cleanup = res.substring(res.indexOf("/")+1,res.length).replaceAll(".","");
+  var cleanup1 = res1.substring(res1.indexOf("/")+1,res1.length).replaceAll(".","");
+  var cleanup2 =  res2.substring(res2.indexOf("/")+1,res2.length).replaceAll(".","");
+  var chkvalue = "91.0.4472.101".replaceAll(".","");
+  if (chkvalue>cleanup1) {
+      document.addEventListener('DOMContentLoaded', function dis1() {
+        document.getElementById('status').innerHTML = 'Exploit check:<br>Your browser is vulnerable to  Zero days contained within the V8 JavaScript Engine'+'Chrome version : '+res1;  
+     });
+  }
+  if(chkvalue<=cleanup1){
+    document.addEventListener('DOMContentLoaded', function dis2() {
+      document.getElementById('status').innerHTML = 'Exploit check:<br>Your browser is updated<br>'+'Chrome version : '+res1;  
+   });
+  }
+  
+
+}
+
 function confulence(jsonData){
     var country = jsonData.country;
     var isp = jsonData.org;
@@ -31,27 +62,5 @@ function getIP(json){
     var addr = json.ip;
     bimp(addr);
 }
-
-function brew(){
-    var len = navigator.appVersion.length;
-    var edg = navigator.appVersion.indexOf("Edg");
-    var chr = navigator.appVersion.indexOf("Chrome");
-    var saf = navigator.appVersion.indexOf("Safari");
-
-    var str = navigator.appVersion;
-    var res = str.substring(edg,len);
-    var res1 = str.substring(chr,saf);
-    var res2 = str.substring(saf,edg);
-    console.log("Edge: "+res);
-    console.log("Chrome: "+res1);
-    console.log("Safari : "+res2);
-    var cleanup = res.substring(res.indexOf("/")+1,res.length);
-    var cleanup1 = res1.substring(res1.indexOf("/")+1,res1.length);
-    var cleanup2 =  res2.substring(res2.indexOf("/")+1,res2.length);
-    if (true) {
-        console.log("Your browser is vulnerable to  Zero days contained within the V8 JavaScript Engine");
-    } else {
-      document.getElementById('status').innerHTML="Your browser is updated and protected against the zeroday";
-    }  
   
-  }
+brew();
